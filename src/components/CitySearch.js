3d0432a9500a4a6f4667e199e4a646ae877375dd
Abrahym Sharf Eldden <asharfeldden@gmail.com>
2020-06-04 13:45:55 -0400

    Added CitySearch Componenet

---
+++ src/components/CitySearch.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+
+class CitySearch extends React.Componenet {
+	constructor(props){
+		super(props);
+	}
+
+	render(){
+		return ();
+	}
+}
