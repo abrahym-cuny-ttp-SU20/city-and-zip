9c9e789358b9c6533fac539536e70d0b4bf91e9f
Marlon-Urena <45586481+Marlon-Urena@users.noreply.github.com>
2020-06-06 00:26:39 +0100

    Performed some basic bug fixes

--- src/components/CitySearch.js
+++ src/components/CitySearch.js
@@ -1,20 +1,17 @@
-import React, {Component} from 'react';
-import axios from 'axios';
+import React from "react";
 
-
-function CitySearch(props){ 
-	console.log(props);
-
-	return (
-		<>
-			<h1>The zip codes in {(props.cityName)} are:< /h1>
-				
-			<ul>
-				{props.zipCodes.map(curZipCode => 
-					<li key={curZipCode}>{curZipCode}</li>) }
-			</ul>
-		</>
-	);
+function CitySearch(props) {
+  console.log(props);
+  return (
+    <>
+      <h1>The zip codes in {props.cityName} are:</h1>
+      <ul>
+        {props.zipCodes.map((curZipCode) => (
+          <li key={curZipCode}>{curZipCode}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default CitySearch;
