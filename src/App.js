8614014784e814379302d51bd4f92640a4a6e9c7
Abrahym Sharf Eldden <asharfeldden@gmail.com>
2020-06-05 14:21:02 -0400

    i

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 import CitySearch from './components/CitySearch.js';
 
-class App extends React.Component{
-    render(){
-      return <CitySearch cityName="nyc" />;
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      cityName: "",
+      zipCodes: [],
+    };
+  }
+
+  handleChange = (e) => {
+    const cityName = e.target.value.toUpperCase();
+
+    axios
+      .get(`http://ctp-zip-api.herokuapp.com/city/${cityName}`)
+      .then((res) => {
+        const zipCodes = res.data;
+
+        this.setState({
+          cityName,
+          zipCodes,
+        });
+      }).catch(err => console.log(err));
+
+
+  }
+
+    render() {
+      return (
+        <> 
+          <label>Please enter a city: </label>
+
+          <input 
+            type="text"
+            onChange={this.handleChange} />
+
+          <CitySearch 
+            cityName={this.state.cityName} 
+            zipCodes={this.state.zipCodes} />
+        </>
+      );
     }
 }
 
--- src/components/CitySearch.js
+++ src/components/CitySearch.js
@@ -1,32 +1,20 @@
-import React from 'react';
+import React, {Component} from 'react';
 import axios from 'axios';
 
-class CitySearch extends React.Component {
-	constructor(props){
-		super(props);
 
-		this.state = { zipCodes: [] };
-	}
+function CitySearch(props){ 
+	console.log(props);
 
-	componentDidMount() {
-		axios
-			.get(`http://ctp-zip-api.herokuapp.com/city/${(this.props.cityName).toUpperCase()}`)
-			.then(res => {
-				const { data } = res;
-
-				this.setState({ zipCodes: data})
-			})
-			.catch(e => console.log(e) );
-	}
-
-	render(){
-		return (
+	return (
+		<>
+			<h1>The zip codes in {(props.cityName)} are:< /h1>
+				
 			<ul>
-				{this.state.zipCodes.map(curZipCode => 
-					<ZipcodeCard key={curZipCode}>{curZipCode} />)}
+				{props.zipCodes.map(curZipCode => 
+					<li key={curZipCode}>{curZipCode}</li>) }
 			</ul>
-		);
-	}
+		</>
+	);
 }
 
 export default CitySearch;
